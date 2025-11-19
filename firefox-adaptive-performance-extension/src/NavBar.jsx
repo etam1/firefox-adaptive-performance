2c6751305825132f2b6d7220a44ef285356e9cbe
etam1 <etam1@berkeley.edu>
2025-11-18 22:07:36 -0800

    nav bar update

--- firefox-adaptive-performance-extension/src/NavBar.jsx
+++ firefox-adaptive-performance-extension/src/NavBar.jsx
@@ -3,6 +3,10 @@ import styles from './NavBar.module.css'
 import Home from './pages/home/Home.jsx'
 import Tabs from './pages/tabs/Tabs.jsx'
 import Settings from './pages/settings/Settings.jsx'
+import homeIcon from './assets/home.svg'
+import tabsIcon from './assets/tabs.svg'
+import settingsIcon from './assets/settings.svg'
+import logoIcon from './assets/logo.svg'
 
 function NavBar() {
   const [currentPage, setCurrentPage] = useState('home')
@@ -23,25 +27,30 @@ function NavBar() {
   return (
     <>
       <nav className={styles.navbar}>
-        <button 
-          className={`${styles.navButton} ${currentPage === 'home' ? styles.active : ''}`}
-          onClick={() => setCurrentPage('home')}
-        >
-          Home
-        </button>
-        <button 
-          className={`${styles.navButton} ${currentPage === 'tabs' ? styles.active : ''}`}
-          onClick={() => setCurrentPage('tabs')}
-        >
-          Tabs
-        </button>
-        <button 
-          className={`${styles.navButton} ${currentPage === 'settings' ? styles.active : ''}`}
-          onClick={() => setCurrentPage('settings')}
-        >
-          Settings
-        </button>
+        <img src={logoIcon} alt="Logo" className={styles.logo} />
+        <div className={styles.navButtons}>
+          <button 
+            className={`${styles.navButton} ${currentPage === 'home' ? styles.active : ''}`}
+            onClick={() => setCurrentPage('home')}
+          >
+            <img src={homeIcon} alt="Home" />
+          </button>
+          <button 
+            className={`${styles.navButton} ${currentPage === 'tabs' ? styles.active : ''}`}
+            onClick={() => setCurrentPage('tabs')}
+          >
+            <img src={tabsIcon} alt="Tabs" />
+          </button>
+          <button 
+            className={`${styles.navButton} ${currentPage === 'settings' ? styles.active : ''}`}
+            onClick={() => setCurrentPage('settings')}
+          >
+            <img src={settingsIcon} alt="Settings" />
+          </button>
+        </div>
       </nav>
+
+      {/* This shows the current page - Home, Tabs, or Settings */}
       <div className={styles.content}>
         {renderPage()}
       </div>
