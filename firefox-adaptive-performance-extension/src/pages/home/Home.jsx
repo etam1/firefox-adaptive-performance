cb4796fb080f6afd089bfc3e83d83b9a0a1bf4fd
plaanty <145934497+plaanty@users.noreply.github.com>
2025-11-21 22:57:35 -0800

    exploring codebase

--- firefox-adaptive-performance-extension/src/pages/home/Home.jsx
+++ firefox-adaptive-performance-extension/src/pages/home/Home.jsx
@@ -1,12 +1,27 @@
 import styles from './Home.module.css'
+import IconLogo from '@/assets/icons/icon-logo.png';
+import { IoSettingsOutline } from "react-icons/io5";
+
 
 function Home() {
   return (
     <div className={styles.home}>
+      
+      <div className={styles.topBar}>
+        <img src={IconLogo} width={24} height={24} alt='Logo' />
+        
+        <div className={styles.topBarRight}>
+          <IoSettingsOutline size={24} />
+        </div>
+      </div>
+
       <h1>Home</h1>
       <p>ethan was here</p>
       <p>everyone zelle ethan 10000 dollars @312-493-0018</p>
+      <p>Testing 123</p>
     </div>
+
+
   )
 }
 
