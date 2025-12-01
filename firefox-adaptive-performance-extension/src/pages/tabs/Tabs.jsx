a1b772a0500877088cb98a538e6b41249a975ba0
daniellee1429 <dl5644@berkeley.edu>
2025-11-30 18:23:57 -0800

    done with memory popup assignment

--- firefox-adaptive-performance-extension/src/pages/tabs/Tabs.jsx
+++ firefox-adaptive-performance-extension/src/pages/tabs/Tabs.jsx
@@ -1,13 +1,20 @@
-import styles from './Tabs.module.css'
+import styles from "./Tabs.module.css";
+import MemoryPopUp from "../../components/PopUps/MemoryPopUp/MemoryPopUp.jsx";
 
 function Tabs() {
   return (
     <div className={styles.tabs}>
       <h1>Tabs</h1>
       <p>Manage your browser tabs here</p>
+
+      {/* Show your popup here */}
+      <MemoryPopUp variant="high" />
+
+      {/* You can add more if you want */}
+      {/* <MemoryPopUp variant="medium" /> */}
+      {/* <MemoryPopUp variant="low" /> */}
     </div>
-  )
+  );
 }
 
-export default Tabs
-
+export default Tabs;
