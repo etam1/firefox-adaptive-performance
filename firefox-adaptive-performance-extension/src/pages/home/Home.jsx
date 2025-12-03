a8d9fd1e8c647ad393878203b86da1561a7353c7
plaanty <145934497+plaanty@users.noreply.github.com>
2025-12-02 21:46:52 -0800

    Making some changes

--- firefox-adaptive-performance-extension/src/pages/home/Home.jsx
+++ firefox-adaptive-performance-extension/src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import styles from './Home.module.css'
 import IconLogo from '@/assets/icons/icon-logo.png';
+import { Accordion } from 'radix-ui';
 import { IoSettingsOutline } from "react-icons/io5";
+import AccordionDemo from '../../components/SuggestedActions/SuggestedActions';
 
 
 function Home() {
@@ -19,6 +21,9 @@ function Home() {
       <p>ethan was here</p>
       <p>everyone zelle ethan 10000 dollars @312-493-0018</p>
       <p>Testing 123</p>
+      <AccordionDemo />
+
+
     </div>
 
 
